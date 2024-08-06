e886fc50bec0ef7344c73e0f1e71e36ea6849195
Mihirv2M <mihirvbrainerhub@gmail.com>
2024-08-06 18:57:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -64,6 +64,6 @@ server.delete("/delete/:index", (req,res) =>{
     let sql = "DELETE FROM games WHERE idgames = ?"
     db.query(sql, [index], (err,result) =>{err ? console.log(err) : res.send(result)})
 })
-server.listen(3001, () =>
+server.listen(3000, () =>
     console.log("Running in the port 3001")
 );
