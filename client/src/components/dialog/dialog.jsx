4523a38a057b6f50d42cc2e408bf931faab7aeac
Mihirv2M <mihirvbrainerhub@gmail.com>
2024-08-06 17:40:40 +0530

    Update dialog.jsx

--- client/src/components/dialog/dialog.jsx
+++ client/src/components/dialog/dialog.jsx
@@ -19,7 +19,7 @@ export default function FormDialog(props) {
 
     const handleEditValues = () => {
         console.log(props.baseUrl)
-        axios.put(`http://3.80.75.38:3001/edit`, {
+        axios.put(`http://3.80.75.38:3000/edit`, {
             id: editValues.id,
             name: editValues.name,
             cost: editValues.cost,
@@ -30,7 +30,7 @@ export default function FormDialog(props) {
     }
 
     const handleDeleteGame = () => {
-        axios.delete(`http://3.80.75.38:3001/delete/${editValues.id}`)
+        axios.delete(`http://3.80.75.38:3000/delete/${editValues.id}`)
     }
 
     const handleChangeValues = (value)=>{
