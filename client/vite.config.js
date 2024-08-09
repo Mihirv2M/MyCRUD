f4a3ca4bfe8b5a00415b57c43c326c7e9d0000eb
Mihirv2M <mihirvbrainerhub@gmail.com>
2024-08-09 16:49:43 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: '/front/', // Set the base path for your project
+
+  server: {
+    proxy: {
+      '/front': {
+        target: 'http://localhost:3000', // Change to your backend's URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/front/, ''),
+      },
+    },
+  },
+});
